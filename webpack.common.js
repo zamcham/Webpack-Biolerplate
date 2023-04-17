5d8397dae4be9f6f0dad7a636fdaa2991ff388ca
zamcham <cristian@zamcham.com>
2023-04-16 23:30:34 -0400

    Add html/files config and clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,8 @@
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
@@ -17,6 +13,14 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        type: "asset/resource",
       }
     ]
   }
