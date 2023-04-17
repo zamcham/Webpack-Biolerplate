f4a53022a240674df76a45de654bd4e18108bdca
zamcham <cristian@zamcham.com>
2023-04-16 22:17:32 -0400

    Cache busting and htmlwebpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[contenthash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
